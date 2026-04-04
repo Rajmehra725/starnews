1954d4a6259d1376442587f413d5c18f3bc7add5
Rajmehra725 <nagiyaraj.725@gmail.com>
2026-04-04 18:13:56 +0530

    signal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 import MainLayout from "./MainLayout";
 import BottomTicker from "./components/BottomTicker";
@@ -32,6 +33,29 @@ export default function RootLayout({
       lang="hi"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* OneSignal SDK */}
+        <Script
+          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+          strategy="afterInteractive"
+        />
+
+        {/* OneSignal Init */}
+        <Script id="onesignal-init" strategy="afterInteractive">
+          {`
+            window.OneSignalDeferred = window.OneSignalDeferred || [];
+            OneSignalDeferred.push(async function(OneSignal) {
+              await OneSignal.init({
+                appId: "5084b9c1-5107-4b55-a60c-72b44ca306b1",
+              });
+
+              // Optional: popup trigger
+              OneSignal.Slidedown.promptPush();
+            });
+          `}
+        </Script>
+      </head>
+
       <body className="min-h-screen bg-gray-100 dark:bg-gray-900 transition">
         <MainLayout>{children}</MainLayout>
         <BottomTicker />
